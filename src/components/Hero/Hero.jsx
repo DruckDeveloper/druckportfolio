54153d18a43132362af8834b48546c65d155e8cf
DruckDeveloper <druckdev@gmail.com>
2023-02-04 15:44:01 -0500

    building aboutme section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useContext} from "react";
+import LangContext from '../../context/LangContext'; 
+import { FormattedMessage } from "react-intl";
+
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const { locale } = useContext(LangContext)
+  let typingClass = locale == 'es-CO' ? 'esTyping' : 'enTyping';  
 
-export default Hero
+  return (
+    <article className="hero">
+      <div className="hero__content">
+        <h2 className={`hero__content--name ${typingClass}`}>
+          <FormattedMessage
+            id="hero.content-name"
+            defaultMessage="Hi, Im Dylan"
+          />
+        </h2>
+
+      </div>
+      <div className="hero__image">
+        <p>hola mundo</p>
+      </div>
+    </article>
+  );
+};
+
+export default Hero;
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero/Hero'
 import { FormattedMessage } from 'react-intl'
+
 
 const AboutMe = () => {
   return (
     <section className='aboutme__section'>
-        hola mundo
+      <Hero />
     </section>
   )
 }
